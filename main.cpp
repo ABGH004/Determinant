71ae6a4fbbde9ee2a462e7111e52b73042a64d2c
ABGH004 <bagheri.alireza.2003@gmail.com>
2023-12-16 19:42:08 +0100

    initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+//gauss-elimination
+void determinant_first_approach();
+//
+void determinant_second_approach();
+//rezaeifar
+void determinant_third_approach();
+
+
+
+int main()
+{
+
+	return 0;
+}
